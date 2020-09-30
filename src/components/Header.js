fd2e6013300b4080eb7f233dcb21e7975aa37a14
John Afolabi <johnafolabi.da@gmail.com>
2020-09-30 18:16:06 +0100

    Design Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,98 +1,76 @@
-import logo from "../img/svg/logo.svg";
-import google from "../img/svg/brands/google.svg";
-import verge from "../img/svg/brands/verge.svg";
-import slack from "../img/svg/brands/slack.svg";
-import boost from "../img/svg/to-the-moon.svg";
+import logo from "../img/png/logo.png";
+import hero from "../img/svg/hero.svg";
 
 export const Header = (open) => {
 	const template = `
     <div class="mobile-nav">
-			<nav class="links-nav">
-				<ul>
-					<li><a href="#about">About</a></li>
-					<li><a href="#packages">Growth Packages</a></li>
-					<li><a href="#schedule">Schedule Strategy Call</a></li>
-					<li><a href="#blog">Blog</a></li>
-				</ul>
-				<button class="nav-cta">Sign Up</button>
-			</nav>
-			<button class="hamburger">menu</button>
-		</div>
+	<nav class="links-nav">
+		<ul>
+			<li><a href="#homr">Home</a></li>
+			<li><a href="#about-us">About Us</a></li>
+			<li><a href="#what-we-do">What We Do</a></li>
+		</ul>
+		<button class="nav-cta">Hire Us</button>
+	</nav>
+	<button class="hamburger">menu</button>
+</div>
 
-		<header id="home">
-			<div class="header">
-				<nav class="nav">
-					<img src="${logo}" class="brand-logo" />
+<header id="home">
+	<div class="header">
+		<nav class="nav">
+			<img src="${logo}" class="brand-logo" />
 
-					<ul>
-						<li class="nav-list">
-							<a href="#about" class="nav-link">About</a>
-						</li>
-						<li class="nav-list">
-							<a href="#packages" class="nav-link"
-								>Growth Packages</a
-							>
-						</li>
-						<li class="nav-list">
-							<a href="#schedule" class="nav-link"
-								>Schedule Strategy Call</a
-							>
-						</li>
-						<li class="nav-list">
-							<a href="#blog" class="nav-link">Blog</a>
-						</li>
-					</ul>
+			<ul>
+				<li class="nav-list">
+					<a href="#home" class="nav-link">Home</a>
+				</li>
+				<li class="nav-list">
+					<a href="#about-us" class="nav-link">About Us</a>
+				</li>
+				<li class="nav-list">
+					<a href="#what-we-do" class="nav-link">What We Do</a>
+				</li>
 
-					<button class="btn-primary">Sign Up</button>
-				</nav>
+				<li><button class="btn-main">Hire Us</button></li>
+			</ul>
 
-				<div class="hero-bg">
-					<div class="hero">
-						<div class="hero-text">
-							<h2>
-								Grow with world-class Reddit marketing services
-							</h2>
-							<p>
-								Achieve your marketing goals on Reddit. Go
-								viral, improve your reputation & more
-							</p>
-							<button class="btn-primary">Get Started</button>
+			
+		</nav>
 
-							<div class="brands">
-								<img src="${verge}" alt="verge" />
-								<img src="${slack}" alt="slack" />
-								<img src="${google}" alt="google" />
-							</div>
-							<div class="summary-text">
-								<span>Start for free</span>
-								<span>Add users for $10/month</span>
-								<span
-									>Add Powerful Features for less than
-									$10/month</span
-								>
-							</div>
-						</div>
-						<img
-							src="${boost}"
-							alt="reddit boost"
-							class="hero-image"
-						/>
-					</div>
+		<div class="hero-bg">
+			<div class="hero">
+				<div class="hero-text">
+					<h2>We provide a comprehensive debt collection service</h2>
+					<p>
+						Our professionals have a combined 85 years in debt
+						recovery efforts that have seen the recovery of more
+						than N19Billion in debt from very unwilling debtors. We
+						use the most advanced techniques to trace debtors and
+						recover money owed. We also offer an alternative method
+						for loan recoveries by allowing debtors provide value
+						worth the amount they are unable to pay.
+					</p>
+					<button class="btn-primary">Hire Us</button
+					><a class="link-hero">Watch the Video</a>
 				</div>
-				<svg
-					id="bottom-curve"
-					width="100%"
-					height="100"
-					viewBox="0 0 1331 206"
-					xmlns="http://www.w3.org/2000/svg"
-					preserveAspectRatio="none"
-				>
-					<path
-						d="M0 0H1331L155.647 203.104C74.3446 217.153 0 154.554 0 72.0462V0Z"
-					/>
-				</svg>
+				<img src="${hero}" alt="debt collection" class="hero-image" />
 			</div>
-		</header>
+		</div>
+		<!-- <svg
+			id="bottom-curve"
+			width="100%"
+			height="100"
+			viewBox="0 0 1331 206"
+			xmlns="http://www.w3.org/2000/svg"
+			preserveAspectRatio="none"
+		>
+			<path
+				d="M0 0H1331L155.647 203.104C74.3446 217.153 0 154.554 0 72.0462V0Z"
+			/>
+		</svg> -->
+	</div>
+</header>
+
     `;
 
 	return template;
